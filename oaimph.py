a60bf15665c75c26f3eb27bc1732022187c4d483
Anh Tuan <Nguyen@carekura.de>
2017-01-06 14:50:20 +0000

    feat: harvesting is a separate method now

--- oaimph.py
+++ oaimph.py
@@ -10,6 +10,34 @@ TODO
 how to process records?
 
 '''
+def harvestOAI(link,startDate=None, endDate=None):
+
+    '''
+    harvest Records from given repository
+    :param link: Link to OAI-MPH repository
+    :param startDate: search for records from given Date
+    :param endDate:  search for records until given Date
+    :return:
+    '''
+
+    # init connection to OAI provider
+    sickle = Sickle(link)
+
+    # for every of the OAI verbs (ListRecords, GetRecord, Idenitfy, ListSets, ListMetadataFormats, ListIdentifiers)
+    # there is a separate sickle method
+    try:
+        records = sickle.ListRecords(**{'metadataPrefix': 'oai_dc', 'from': startDate, 'until': endDate})
+    except oaiexceptions.NoRecordsMatch:
+        print("No Records found for the the given criteria")
+    except exceptions.MissingSchema:
+        print("Link", link, "is not a valid link")
+    except exceptions.ConnectionError:
+        print("Link", link, "cannot be connected")
+    except oaiexceptions.BadArgument:
+        print("Invalid Parameters")
+    else:
+        for record in records:
+            print(record)
 
 link1 = 'http://citeseerx.ist.psu.edu/oai2'
 link2 = 'http://export.arxiv.org/oai2'
@@ -17,33 +45,6 @@ link3 = 'http://elis.da.ulcc.ac.uk/cgi/oai2'
 
 startDate = '2016-01-10'
 endDate = '2017-01-06'
-
-#init connection to OAI provider
 link = link1
 
-sickle = Sickle(link)
-
-# for every of the OAI verbs (ListRecords, GetRecord, Idenitfy, ListSets, ListMetadataFormats, ListIdentifiers)
-# there is a separate sickle method
-exception = False
-
-
-try:
-    records = sickle.ListRecords(**{'metadataPrefix': 'oai_dc', 'from': startDate, 'until': endDate })
-except oaiexceptions.NoRecordsMatch:
-    print("No Records found for the the given criteria")
-    exception = True
-except exceptions.MissingSchema:
-    print("Link", link, "is not a valid link")
-    exception = True
-except exceptions.ConnectionError:
-    print("Link", link, "cannot be connected")
-    exception = True
-except oaiexceptions.BadArgument:
-    print("Invalid Parameters")
-    exception = True
-
-if exception is False:
-    for record in records:
-        print(record)
-
+harvestOAI(link,endDate=endDate)
