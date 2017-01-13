501640988f34dec2575811c0c644aad5b4aa6d73
Anh Tuan Nguyen <nguyen@carekura.de>
2017-01-13 16:54:16 +0100

    added example for mariadb insertion

---
+++ mariadb_insert_data.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+from datetime import date, datetime, timedelta
+import mysql.connector
+
+cnx = mysql.connector.connect(user='root',
+                              password='master',
+                              host='127.0.0.1',
+                              database='employees'
+                              )
+cursor = cnx.cursor()
+
+tomorrow = datetime.now().date() + timedelta(days=1)
+
+
+#sql schema for new data
+add_employee = ("INSERT INTO employees "
+               "(first_name, last_name, hire_date, gender, birth_date) "
+               "VALUES (%s, %s, %s, %s, %s)")
+add_salary = ("INSERT INTO salaries "
+              "(emp_no, salary, from_date, to_date) "
+              "VALUES (%(emp_no)s, %(salary)s, %(from_date)s, %(to_date)s)")
+
+#actual data to be inserted
+data_employee = ('Geert', 'Vanderkelen', tomorrow, 'M', date(1977, 6, 14))
+
+# Insert new employee
+cursor.execute(add_employee, data_employee)
+# get ID of last row for foreign key
+emp_no = cursor.lastrowid
+
+# Insert salary information
+data_salary = {
+  'emp_no': emp_no,
+  'salary': 50000,
+  'from_date': tomorrow,
+  'to_date': date(9999, 1, 1),
+}
+cursor.execute(add_salary, data_salary)
+
+# Make sure data is committed to the database
+#commit is useful for finishing a transactional action
+cnx.commit()
+
+cursor.close()
+cnx.close()
