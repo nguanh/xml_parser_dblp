ecc227a7b7695448fa7437e52d2b7afdf618b8e8
Anh Tuan <Nguyen@carekura.de>
2017-02-01 12:58:04 +0000

    testing celery stuff

--- proj/celery.py
+++ proj/celery.py
@@ -10,7 +10,7 @@ app = Celery('proj',
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     # Calls test('hello') every 10 seconds.
-    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
+    sender.add_periodic_task(10.0, test.delay('hello'), name='add every 10')
     # Calls test('world') every 30 seconds
     sender.add_periodic_task(30.0, test.s('world'), expires=10)
 
