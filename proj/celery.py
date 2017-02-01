9f59269a6cfe794981868bebaf92bab8ceaab8c1
Anh Tuan <Nguyen@carekura.de>
2017-02-01 12:31:23 +0000

    feat: added test for celery

--- proj/celery.py
+++ proj/celery.py
@@ -1,20 +1,37 @@
 from __future__ import absolute_import, unicode_literals
 from celery import Celery
+from celery.schedules import crontab
 
 app = Celery('proj',
              broker='amqp://',
              backend='amqp://',
              include=['proj.tasks'])
 
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Calls test('hello') every 10 seconds.
+    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
+
+    # Calls test('world') every 30 seconds
+    sender.add_periodic_task(30.0, test.s('world'), expires=10)
+
+    # Executes every Monday morning at 7:30 a.m.
+    sender.add_periodic_task(
+        crontab(hour=7, minute=30, day_of_week=1),
+        test.s('Happy Mondays!'),
+    )
+
 # Optional configuration, see the application user guide.
-app.conf.update(
-    result_expires=3600,
-)
+# app.conf.update(
+#     result_expires=3600,
+# )
 
 if __name__ == '__main__':
     app.start()
 
-
+@app.task
+def test(arg):
+    print(arg)
 # TODO
 # import celery config file
 # app.config_from_object('celeryconfig')
