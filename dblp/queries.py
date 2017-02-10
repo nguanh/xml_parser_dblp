5c5e5c788a56d187677e348b9cda97dade87db80
Anh Tuan <Nguyen@carekura.de>
2017-02-10 16:41:02 +0000

    update duplicate

--- dblp/queries.py
+++ dblp/queries.py
@@ -56,5 +56,23 @@ DBLP_ARTICLE = (
     ") ENGINE=InnoDB CHARSET=utf8mb4")
 
 ADD_DBLP_ARTICLE = ("INSERT INTO dblp_article"
-                    " (dblp_key,mdate, author,title,pages,pub_year,volume,journal,journal_number,ee,url,cite,crossref,booktitle,last_updated,last_harvested) "
-                    "VALUES ( %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s, CURRENT_TIMESTAMP, 0 )")
+                    " (dblp_key,mdate, author,title,pages,pub_year,volume,journal,journal_number,"
+                    "ee,url,cite,crossref,booktitle,last_updated,last_harvested) "
+                    "VALUES ( %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s, CURRENT_TIMESTAMP, 0 )"
+                    "ON DUPLICATE KEY UPDATE last_updated= CURRENT_TIMESTAMP,"
+                    "mdate =VALUES(mdate),"
+                    "author =VALUES(author),"
+                    "title =VALUES(title),"
+                    "pages =VALUES(pages),"
+                    "pub_year =VALUES(pub_year),"
+                    "volume =VALUES(volume),"
+                    "journal =VALUES(journal_number),"
+                    "ee =VALUES(ee),"
+                    "url =VALUES(url),"
+                    "cite =VALUES(cite),"
+                    "crossref =VALUES(crossref),"
+                    "booktitle =VALUES(booktitle)"
+                     )
+
+
+
