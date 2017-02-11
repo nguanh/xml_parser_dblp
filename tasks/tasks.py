e1dc275e7f27d9da304f6ec8b55b593333a97a70
Anh Tuan <Nguyen@carekura.de>
2017-02-11 14:31:06 +0000

    task test

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import, unicode_literals
+#from __future__ import absolute_import, unicode_literals
 from .celery import app
 from logs.config import LOG_CONFIG
 from celery.utils.log import get_task_logger
