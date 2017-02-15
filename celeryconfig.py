c47b1682d4c56149f4d2ae39ff19a4585fce97df
Anh Tuan <Nguyen@carekura.de>
2017-02-15 15:51:17 +0000

    minor oai bug fix and arxiv test

--- celeryconfig.py
+++ celeryconfig.py
@@ -28,7 +28,7 @@ beat_schedule = {
     },
     'arxiv-harvester': {
         'task': 'tasks.tasks.harvest_source',
-        'schedule': crontab(minute=17, hour=14),
+        'schedule': crontab(minute=52, hour=15),
         'args': ("oai.arxivharvester", "ArXivHarvester", "ARXIV_HARVESTER")
     }
 }
