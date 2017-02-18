b3bc39c8cc6e68a5e9101c7e045cf9d8f7d19851
Anh Tuan <Nguyen@carekura.de>
2017-02-18 10:48:25 +0000

    fix: parse_authors return correct list now

--- pub_storage/helper.py
+++ pub_storage/helper.py
@@ -6,7 +6,9 @@ punctuation_dict = str.maketrans({key: None for key in (string.punctuation)})
 whitespace_dict = str.maketrans({key: None for key in (string.whitespace.replace(" ", ""))})
 ascii_dict = str.maketrans({key: None for key in (string.printable)})
 
-#TODO interpret latex commands
+# TODO interpret latex commands
+
+
 def normalize_title(title):
 
     # translate unicode characters to closest ascii characters
@@ -21,29 +23,31 @@ def normalize_title(title):
 
     return only_one_space.strip()
 
-#TODO return list
-def parse_authors(input):
-    authors_list= input.split(";")
-    #remove last entry since its always empty
+
+def parse_authors(author_csv):
+    authors_list= author_csv.split(";")
+    # remove last entry since its always empty
     del authors_list[-1]
+    names = []
     for author in authors_list:
-        names = {'original': author}
+        name_dict = {'original': author}
         # remove dblp numbers from name
         stripped_numbers = re.sub(r'\d{4}', '', author).strip()
         # try to find last name
         num_names = len(author.split(" "))
-        names['real'] = stripped_numbers
+        name_dict['real'] = stripped_numbers
         # if author name contains only one name, it will serve the purpose of the last name
-        if( num_names == 1):
+        if num_names == 1:
             last_name = stripped_numbers
-            names['block'] = normalize_title(last_name) + ","
+            name_dict['block'] = normalize_title(last_name) + ","
         else:
             name = HumanName(stripped_numbers).as_dict()
             norm_last_name = normalize_title(name['last'])
             norm_first_name = normalize_title(name['first'])[0] if len(name['first']) > 0 else ''
-            names['block'] = norm_last_name + "," + norm_first_name
-            #TODO generate alias
-            #TODO brauchen überhaupt aliase generiert werden?
+            name_dict['block'] = norm_last_name + "," + norm_first_name
 
+        names.append(name_dict)
 
     return names
+
+
--- tests/test_parse_authors.py
+++ tests/test_parse_authors.py
@@ -3,56 +3,55 @@ from pub_storage.helper import parse_authors
 
 class TestParse_authors(TestCase):
     def test_parse_authors1(self):
-        name = parse_authors("Walter Vogler;")
+        name = parse_authors("Walter Vogler;")[0]
         self.assertEqual(name['original'], "Walter Vogler")
         self.assertEqual(name['real'], "Walter Vogler")
         self.assertEqual(name['block'], "vogler,w")
 
     def test_parse_authors2(self):
-        test = parse_authors("Richard Müller 0001;Alexander S. szalay;")
-        name = parse_authors("Richard Müller 0001;")
+        name = parse_authors("Richard Müller 0001;")[0]
         self.assertEqual(name['original'], "Richard Müller 0001")
         self.assertEqual(name['real'], "Richard Müller")
         self.assertEqual(name['block'], "muller,r")
 
     def test_parse_authors3(self):
-        name = parse_authors("Alexander S. szalay 0501;")
+        name = parse_authors("Alexander S. szalay 0501;")[0]
         self.assertEqual(name['original'], "Alexander S. szalay 0501")
         self.assertEqual(name['real'], "Alexander S. szalay")
         self.assertEqual(name['block'], "szalay,a")
 
     def test_parse_authors4(self):
-        name = parse_authors("luqi;")
+        name = parse_authors("luqi;")[0]
         self.assertEqual(name['original'], "luqi")
         self.assertEqual(name['real'], "luqi")
         self.assertEqual(name['block'], "luqi,")
 
     def test_parse_authors5(self):
-        name = parse_authors("Peter van der Stok;")
+        name = parse_authors("Peter van der Stok;")[0]
         self.assertEqual(name['original'], "Peter van der Stok")
         self.assertEqual(name['real'], "Peter van der Stok")
         self.assertEqual(name['block'], "van der stok,p")
 
     def test_parse_authors6(self):
-        name = parse_authors("Yi-Ping Phoebe Chen;")
+        name = parse_authors("Yi-Ping Phoebe Chen;")[0]
         self.assertEqual(name['original'], "Yi-Ping Phoebe Chen")
         self.assertEqual(name['real'], "Yi-Ping Phoebe Chen")
         self.assertEqual(name['block'], "chen,y")
 
     def test_parse_authors7(self):
-        name = parse_authors("Jóhanna V. Gísladóttir;")
+        name = parse_authors("Jóhanna V. Gísladóttir;")[0]
         self.assertEqual(name['original'], "Jóhanna V. Gísladóttir")
         self.assertEqual(name['real'], "Jóhanna V. Gísladóttir")
         self.assertEqual(name['block'], "gisladottir,j")
 
     def test_parse_authors8(self):
-        name = parse_authors("Andreas Giskeødegård;")
+        name = parse_authors("Andreas Giskeødegård;")[0]
         self.assertEqual(name['original'], "Andreas Giskeødegård")
         self.assertEqual(name['real'], "Andreas Giskeødegård")
         self.assertEqual(name['block'], "giskeodegard,a")
 
     def test_parse_authors9(self):
-        name = parse_authors("Ola R. Snøve Jr;")
+        name = parse_authors("Ola R. Snøve Jr;")[0]
         self.assertEqual(name['original'], "Ola R. Snøve Jr")
         self.assertEqual(name['real'], "Ola R. Snøve Jr")
         self.assertEqual(name['block'], "snove,o")
