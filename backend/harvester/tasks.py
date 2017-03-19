2f717b2a1116fabc13e623bd8609a79dc398d64a
Anh Tuan <Nguyen@carekura.de>
2017-03-19 22:07:55 +0000

    experimental:django harvester added

--- backend/harvester/tasks.py
+++ backend/harvester/tasks.py
@@ -1,18 +1,42 @@
 # Create your tasks here
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
-
+from celery.exceptions import Ignore
+from celery import states
+from harvester.exception import IHarvest_Disabled,IHarvest_Exception
 
 @shared_task
 def add(x, y):
     return x + y
 
-
 @shared_task
-def mul(x, y):
-    return x * y
+def harvest_source(package, class_name, name, **parameters):
+    """
 
-
-@shared_task
-def xsum(numbers):
-    return sum(numbers)
+    :param package: relative path to package
+    :param class_name: class name in package
+    :param name: name of the harvester (important for config)
+    :param parameters: parameters for harvester as dict parameters
+    :return:
+    """
+    try:
+        #harvest_task(package, class_name, name, None)
+        print("hallohallo")
+        return True
+    except ImportError as e:
+        harvest_source.update_state(
+            state=states.FAILURE,
+            meta=e,
+        )
+        raise Ignore()
+    except IHarvest_Exception as e:
+        harvest_source.update_state(
+            state=states.FAILURE,
+            meta=e
+        )
+    except IHarvest_Disabled:
+        # task is disabled
+        harvest_source.update_state(
+            state=states.SUCCESS,
+            meta=''
+        )
