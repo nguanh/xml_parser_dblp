b603d4287114f045282f04d3388beee4d156e183
Anh Tuan <Nguyen@carekura.de>
2017-03-20 11:45:44 +0000

    _ in task names cause errors

--- backend/harvester/tasks.py
+++ backend/harvester/tasks.py
@@ -12,23 +12,8 @@ def test():
     print ("Hello")
 
 
-@shared_task()
-def printyy(a,b):
-    print(a,b)
-
-
 @shared_task
-def add(x, y):
-    print(str(x+y))
-    return x + y
-
-@shared_task()
-def test_me():
-    print ("Hello")
-
-'''
-@shared_task
-def harvest_source(package, class_name, name, **parameters):
+def harvestSource(package, class_name, name, **parameters):
     """
 
     :param package: relative path to package
@@ -40,20 +25,19 @@ def harvest_source(package, class_name, name, **parameters):
     try:
         harvest_task(package, class_name, name, None)
     except ImportError as e:
-        harvest_source.update_state(
+        harvestSource.update_state(
             state=states.FAILURE,
             meta=e,
         )
         raise Ignore()
     except IHarvest_Exception as e:
-        harvest_source.update_state(
+        harvestSource.update_state(
             state=states.FAILURE,
             meta=e
         )
     except IHarvest_Disabled:
         # task is disabled
-        harvest_source.update_state(
+        harvestSource.update_state(
             state=states.SUCCESS,
             meta=''
         )
-'''
