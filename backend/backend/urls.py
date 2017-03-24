493484023d88e4adc279c17bb99c8ddf396c5f89
Anh Tuan <Nguyen@carekura.de>
2017-03-24 16:30:58 +0000

    changed admin url order

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^admin/harvester/', include('harvester.urls')),  # harvester admin
+    url(r'^admin/', admin.site.urls),
+
 ]
